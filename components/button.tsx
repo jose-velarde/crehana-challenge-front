1fb1298357e628a16499d7fcfd3e44f965240a49
Jose Velarde <josevelarde00@gmail.com>
2021-12-06 01:50:10 -0300

    Added button components

---
+++ components/button.tsx
@@ -0,0 +1,35 @@
+const Button = ({
+  outline,
+  disabled,
+  children,
+  className = '',
+  type,
+  onClick,
+}: {
+  outline?: boolean
+  disabled?: boolean
+  children: string | JSX.Element
+  className?: string
+  type?: 'button' | 'submit' | 'reset' | undefined
+  onClick?: () => void
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+      className={
+        disabled
+          ? ' pointer-events-none w-full px-4 py-2 text-xl font-semibold text-gray-400 bg-white border-2 border-gray-400 rounded-xl '
+          : (outline
+              ? 'w-full px-4 py-2 text-xl font-semibold text-gray-900 transition-all duration-200 ease-linear bg-white border-2 border-gray-900 rounded-xl hover:bg-gray-900 hover:text-white'
+              : 'w-full px-4 py-2 text-xl font-semibold text-white transition-all duration-200 ease-linear bg-gray-900 rounded-xl hover:bg-gray-500') +
+            className
+      }
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
---
+++ components/buttonLink.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+const ButtonLink = ({
+  path,
+  pathText,
+  outline,
+}: {
+  path: string
+  pathText: string
+  outline?: boolean
+}) => {
+  return (
+    <Link href={path} passHref>
+      <a
+        className={
+          outline
+            ? 'px-4 py-2 text-xl font-semibold text-gray-900 border-2 border-gray-900 transition-all duration-200 ease-linear bg-white rounded-xl hover:bg-gray-900 hover:text-white'
+            : 'px-4 py-2 text-xl font-semibold text-white transition-all duration-200 ease-linear bg-gray-900 rounded-xl hover:bg-gray-500'
+        }
+      >
+        {pathText}
+      </a>
+    </Link>
+  )
+}
+
+export default ButtonLink
