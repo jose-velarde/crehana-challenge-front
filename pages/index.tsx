dda2919339aa881cd0c6678903836c7143c0d08a
Jose Velarde <josevelarde00@gmail.com>
2021-12-11 12:48:56 -0300

    Added requests to Heroku App

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,134 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../components/button'
 import CourseCard from '../components/courseCard'
 import Filter from '../components/filter'
 import Layout from '../components/layout'
-import { Ifilter } from '../types/types'
+import {
+  Ifilter,
+  Icourse,
+  Ipage,
+  Icategory,
+  Isubcategory,
+} from '../types/types'
+import axios from 'axios'
 
-const categories = ['1', '2', '3', '4', '5', '6', '7', '8']
-const courses = [
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  '11',
-  '12',
-  '13',
-  '14',
-  '15',
-]
+axios.defaults.baseURL = process.env.API
 
 const Home: NextPage = () => {
+  const [courses, setCourses] = useState<Icourse[]>([])
+  const [categories, setCategories] = useState<string[]>(['Todos'])
+  const [subcategories, setSubcategories] = useState<string[]>(['Todos'])
+  const [page, setPage] = useState<Ipage>({
+    current_page: 1,
+    total_pages: 1,
+    next_page: 2,
+    previous_page: 1,
+  })
   const [filter, setFilter] = useState<Ifilter>({
     categoryFilter: 'Todos',
     subcategoryFilter: 'Todos',
     levelFilter: 'Todos',
     priceFilter: 'Todos',
   })
-  console.log(filter)
-  const handleSubmit = () => {
-    //apply filter
+
+  const getCategories = () => {
+    axios
+      .get('categories/')
+      .then((response) => {
+        response.data.results.forEach(
+          (category: Icategory) =>
+            setCategories((prevState) => [
+              ...prevState,
+              category.category_name,
+            ])
+        )
+      })
+      .catch((error) => {
+        console.error(error)
+        setCategories(['Todos'])
+      })
   }
+  const getSubcategories = () => {
+    axios
+      .get('subcategories/')
+      .then((response) => {
+        response.data.results.forEach(
+          (subcategory: Isubcategory) =>
+            setSubcategories((prevState) => [
+              ...prevState,
+              subcategory.subcategory_name,
+            ])
+        )
+      })
+      .catch((error) => {
+        console.error(error)
+        setSubcategories(['Todos'])
+      })
+  }
+  const getAndSet = ({
+    pageNumber,
+    filterString = '',
+  }: {
+    pageNumber: number
+    filterString?: string
+  }) => {
+    axios
+      .get('courses/?page=' + pageNumber + filterString)
+      .then((response) => {
+        setPage({
+          ...page,
+          current_page: response.data.page_number,
+          total_pages: response.data.total_pages,
+          next_page: response.data.next,
+          previous_page: response.data.previous,
+        })
+        setCourses(response.data.results)
+      })
+      .catch((error) => {
+        console.error(error)
+        setCourses([])
+      })
+  }
+  useEffect(() => {
+    getCategories()
+    getSubcategories()
+    getAndSet({ pageNumber: 1 })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const changePage = (changeToPage: number) => {
+    getAndSet({ pageNumber: changeToPage })
+  }
+  const applyFilter = () => {
+    let categoryFilterString = ''
+    let subcategoryFilterString = ''
+    let levelFilterString = ''
+    let priceFilterString = ''
+
+    if (filter.categoryFilter != 'Todos') {
+      categoryFilterString = '&category__category_name=' + filter.categoryFilter
+    }
+    if (filter.subcategoryFilter != 'Todos') {
+      subcategoryFilterString =
+        '&subcategory__subcategory_name=' + filter.subcategoryFilter
+    }
+    if (filter.levelFilter != 'Todos') {
+      levelFilterString = '&level=' + filter.levelFilter
+    }
+    if (filter.priceFilter != 'Todos') {
+      priceFilterString = '&price_lte=' + filter.priceFilter
+    }
+
+    const filterToApply =
+      categoryFilterString +
+      subcategoryFilterString +
+      levelFilterString +
+      priceFilterString
+
+    getAndSet({ pageNumber: 1, filterString: filterToApply })
+  }
+
   return (
     <Layout>
       <main className="flex flex-col items-center justify-center overflow-x-hidden gap-y-2">
@@ -47,7 +140,7 @@ const Home: NextPage = () => {
           }}
         >
           <div className="flex flex-col items-center justify-center w-full h-full gap-4 bg-gray-900 bg-opacity-50">
-            <h1 className="text-4xl font-semibold text-center text-white uppercase  md:text-5xl">
+            <h1 className="text-4xl font-semibold text-center text-white uppercase md:text-5xl">
               Title h2 - <br />
               banner cursos nuevos
             </h1>
@@ -60,14 +153,14 @@ const Home: NextPage = () => {
           Title H4 - Listado de categorias
         </div>
         <div className="w-10/12">
-          <div className="grid grid-flow-col overflow-auto overflow-x-auto  gap-x-8">
-            {categories.map((category) => {
+          <div className="grid grid-flow-col overflow-auto overflow-x-auto gap-x-8">
+            {categories.map((category, index) => {
               return (
                 <div
-                  key={category}
+                  key={category + index}
                   className="flex flex-col justify-end h-16 text-center bg-gray-300"
                 >
-                  <div className="py-2 font-bold">Categoria</div>
+                  <div className="py-2 font-bold">{category}</div>
                 </div>
               )
             })}
@@ -78,7 +171,9 @@ const Home: NextPage = () => {
         </div>
         <div className="w-10/12">
           <Filter
-            onSubmit={() => handleSubmit()}
+            onClick={() => applyFilter()}
+            categories={categories}
+            subcategories={subcategories}
             filter={filter}
             setFilter={setFilter}
           />
@@ -87,40 +182,57 @@ const Home: NextPage = () => {
         <div className="grid w-10/12 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-16 ">
           {courses.map((course) => {
             return (
-              <div key={course}>
-                <CourseCard />
+              <div key={course.id}>
+                <CourseCard cardData={course} />
               </div>
             )
           })}
         </div>
         <div className="flex justify-end w-10/12">
-          <div className="inline-flex items-center gap-x-4">
-            <Button disabled={true} outline={true}>{'<'}</Button>
-            <div>Página</div>
-            <div className="relative w-full h-full">
-              <select
-              value="1"
-                className="block w-full h-full pl-6 py-2 mr-12 leading-tight text-gray-600 bg-white border border-gray-400 rounded-lg appearance-none  focus:outline-none focus:bg-white focus:border-gray-500"
+          {page.total_pages == 1 ? null : (
+            <div className="inline-flex items-center gap-x-4">
+              <Button
+                onClick={() => changePage(page.previous_page)}
+                disabled={!page.previous_page}
+                outline={true}
               >
-                <option selected value="1">
-                  1
-                </option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-              <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-400 pointer-events-none">
-                <svg
-                  className="w-4 h-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
+                {'<'}
+              </Button>
+              <div>Página</div>
+              <div className="relative w-full h-full">
+                <select
+                  onChange={(e) => changePage(Number(e.target.value))}
+                  value={page.current_page}
+                  className="block w-full h-full py-2 pl-6 mr-12 leading-tight text-gray-600 bg-white border border-gray-400 rounded-lg appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
                 >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
+                  {[...Array(page.total_pages)].map((undef, page) => {
+                    return (
+                      <option key={page + 1} value={page + 1}>
+                        {page + 1}
+                      </option>
+                    )
+                  })}
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 text-gray-400 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </div>
               </div>
+              <div className=" whitespace-nowrap">de {page.total_pages}</div>
+              <Button
+                onClick={() => changePage(page.next_page)}
+                disabled={!page.next_page}
+                outline={true}
+              >
+                {'>'}
+              </Button>
             </div>
-            <div className=" whitespace-nowrap">de 3</div>
-            <Button outline={true}>{'>'}</Button>
-          </div>
+          )}
         </div>
       </main>
     </Layout>
